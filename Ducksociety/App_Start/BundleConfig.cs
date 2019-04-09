d2dca9e73b9d3ceefea51757619551c4a315774a
Emmanuel Aldoverasss <aldover.asssssssssssssssss@gmail.com>
2019-04-10 01:53:56 +0800

    v1.02

--- Ducksociety/App_Start/BundleConfig.cs
+++ Ducksociety/App_Start/BundleConfig.cs
@@ -11,6 +11,18 @@ namespace Ducksociety
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                           "~/Scripts/jquery.prettyPhoto.js",
+                           "~/Scripts/fontawesome/all.js",
+                           "~/Scripts/fontawesome/all.min.js",
+                           "~/Scripts/fontawesome/brands.js",
+                           "~/Scripts/fontawesome/brands.min.js",
+                           "~/Scripts/fontawesome/fontawesome.js",
+                           "~/Scripts/fontawesome/fontawesome.min.js",
+                           "~/Scripts/fontawesome/regular.js",
+                           "~/Scripts/fontawesome/regular.min.js",
+                           "~/Scripts/fontawesome/solid.js",
+                           "~/Scripts/fontawesome/solid.min.js",
+                           "~/Scripts/fontawesome/v4-shims.js",
+                           "~/Scripts/fontawesome/v4-shims.min.js",
                             "~/Scripts/main.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
@@ -19,10 +31,25 @@ namespace Ducksociety
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
-                      "~/Content//font-awesome.min.css",
-                        "~/Content//prettyPhoto.css",
-                           "~/Content//animate.css",
-                              "~/Content//main.css"));
+                      "~/Content/font-awesome.min.css",
+                        "~/Content/prettyPhoto.css",
+                           "~/Content/animate.css",
+                             "~/Content/brands.css",
+                               "~/Content/fontawesome-all.css",
+                                 "~/Content/fontawesome-all.min.css",
+                                    "~/Content/fontawesome.css",
+                                      "~/Content/fontawesome.min.css",
+                                        "~/Content/main.css",
+                                          "~/Content/prettyPhoto.css",
+                                            "~/Content/regular.css",
+                                              "~/Content/regular.min.css",
+                                                "~/Content/solid.css",
+                                                  "~/Content/solid.min.css",
+                                                    "~/Content/svg-with-js.css",
+                                                      "~/Content/svg-with-js.min.css",
+                                                        "~/Content/v4-shims.css",
+                                                          "~/Content/v4-shims.min.css",
+                                                                 "~/Content/main.css"));
 
 
         }
